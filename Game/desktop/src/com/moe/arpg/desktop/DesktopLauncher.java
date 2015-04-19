0ce7d47de5e67664d89ec5dfbfbccf7990bb3691
moe091 <moe091@gmail.com>
2015-04-19 18:59:45 -0400

    Initial upload

---
+++ Game/desktop/src/com/moe/arpg/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.moe.arpg.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.moe.arpg.GdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1200;
+		config.height = 800;
+		new LwjglApplication(new GdxGame(), config);
+	}
+}
